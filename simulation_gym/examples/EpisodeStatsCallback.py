e114d15e33b97ee4327d227fc5b988298341e8fc
Ahmed <borcheni.ahmed99@gmail.com>
2025-05-13 20:18:16 +0200

    graph for velocity at each episode

--- simulation_gym/examples/EpisodeStatsCallback.py
+++ simulation_gym/examples/EpisodeStatsCallback.py
@@ -1,4 +1,5 @@
 from stable_baselines3.common.callbacks import BaseCallback
+import numpy as np
 
 class EpisodeStatsCallback(BaseCallback):
     def __init__(self, verbose=0):
@@ -6,6 +7,7 @@ class EpisodeStatsCallback(BaseCallback):
         self.episode_rewards = []
         self.episode_lengths = []
         self.current_rewards = []
+        self.episode_velocities = []
         self.episode_counter = 0
 
     def _on_step(self) -> bool:
@@ -23,11 +25,28 @@ class EpisodeStatsCallback(BaseCallback):
             self.episode_lengths.append(ep_length)
             self.episode_counter += 1
 
+
             # Logging (optional)
             print(f"Episode {self.episode_counter}: length={ep_length}, total_reward={ep_reward:.2f}")
 
             # Reset for next episode
             self.current_rewards.clear()
+            # log the distance to the target
+            obs = self.locals["new_obs"]
+
+            # Extract and convert from array([[value]]) → float
+            # x = obs["x"][0][0]
+            # y = obs["y"][0][0]
+            # z = obs["z"][0][0]
+            tx = obs["target_x"][0][0]
+            ty = obs["target_y"][0][0]
+            tz = obs["target_z"][0][0]
+
+            # Compute Euclidean distance
+            distance = np.sqrt((tx)**2 + (ty)**2 + (tz)**2)
+
+            # Append to your array
+            self.episode_velocities.append(float(distance)/ep_length)
 
         return True  # returning False would stop training
 
@@ -35,4 +54,5 @@ class EpisodeStatsCallback(BaseCallback):
         return {
             "episode_rewards": self.episode_rewards,
             "episode_lengths": self.episode_lengths,
+            "episode_velocities": self.episode_velocities,
         }
--- simulation_gym/examples/training_visualiser.py
+++ simulation_gym/examples/training_visualiser.py
@@ -8,12 +8,13 @@ from matplotlib.colors import Normalize
 
 file_path = os.path.join(os.path.dirname(__file__), "training_stats.pkl")
 #file_path = "/Users/MatthiasSchmitt/Desktop/Studium/10 Semester/AI_Project2/bluerov2_gym/simulation_gym/examples/cluster_check_2mio/training_stats.pkl"
+
 with open(file_path, "rb") as f:
     stats = pickle.load(f)
 
 episode_rewards = stats["episode_rewards"]  # list of list of rewards per step
 episode_lengths = stats["episode_lengths"]  # list of lengths per episode
-
+episode_velocities = stats["episode_velocities"]  # list of distances per episode
 
 
 
@@ -25,7 +26,7 @@ total_rewards = [sum(rewards) for rewards in episode_rewards]
 plt.figure(figsize=(18, 5))
 
 # Plot total reward per episode
-plt.subplot(1, 3, 1)
+plt.subplot(1, 4, 1)
 plt.plot(total_rewards)
 plt.xlabel("Episode", fontsize=17)  # Increased fontsize
 plt.ylabel("Total Reward", fontsize=17)  # Increased fontsize
@@ -33,15 +34,16 @@ plt.grid(True)
 plt.tick_params(axis='both', labelsize=14)  # Increased tick number size
 
 # Plot episode lengths
-plt.subplot(1, 3, 2)
+plt.subplot(1, 4, 2)
 plt.plot(episode_lengths, color="C3")
 plt.xlabel("Episode", fontsize=17)  # Increased fontsize
 plt.ylabel("Episode Length (steps)", fontsize=17)  # Increased fontsize
 plt.grid(True)
 plt.tick_params(axis='both', labelsize=14)  # Increased tick number size
 
+
 # Plot reward curves per episode with color scale
-plt.subplot(1, 3, 3)
+plt.subplot(1, 4, 3)
 cmap = plt.get_cmap('viridis')
 N = len(episode_rewards)
 colors = [cmap(i / (N - 1)) for i in range(N)]
@@ -61,6 +63,14 @@ sm = ScalarMappable(cmap=cmap, norm=norm)
 sm.set_array([])  # Dummy array for the colorbar
 cbar = plt.colorbar(sm, ax=plt.gca(), pad=0.02)
 cbar.set_label("Episode Number", fontsize=17)  # Increased fontsize
+
+# Plot episode target distances
+plt.subplot(1, 4, 4)
+plt.plot(episode_velocities, color="C2")
+plt.xlabel("Episode", fontsize=17)  # Increased fontsize
+plt.ylabel("Episode Velocities", fontsize=17)  # Increased fontsize
+plt.grid(True)
+plt.tick_params(axis='both', labelsize=14)  # Increased tick number size
 
 # Remove title
 plt.tight_layout()
